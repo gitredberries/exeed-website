8663cbf70aa505a3f647ecd4ce8c2dcb885cf659
jipinm <jipinm@gmail.com>
2026-05-05 16:31:22 +0530

    Update nuxt.config.ts for Railway deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -53,6 +53,35 @@ export default defineNuxtConfig({
   gtag('config', 'G-LWN11QLX86');`,
           type: "text/javascript",
         },
+        // Google tag (gtag.js) - GA4: G-WNED626QHL
+        { src: "https://www.googletagmanager.com/gtag/js?id=G-WNED626QHL", async: true },
+        {
+          innerHTML: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-WNED626QHL');`,
+          type: "text/javascript",
+        },
+        // Meta Pixel Code
+        {
+          innerHTML: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window,document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '3831278527102130');
+fbq('track', 'PageView');`,
+          type: "text/javascript",
+        },
+      ],
+      noscript: [
+        // Meta Pixel noscript fallback
+        {
+          innerHTML: `<img height="1" width="1" src="https://www.facebook.com/tr?id=3831278527102130&ev=PageView&noscript=1"/>`,
+        },
       ],
     },
   },
